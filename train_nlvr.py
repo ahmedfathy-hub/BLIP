9d4d03aa66ca5284ee667ed52353700a60d32e74
hatemnoaman <hatemnoaman@yahoo.com>
2024-01-26 14:43:06 +0000

    first commit

--- train_nlvr.py
+++ train_nlvr.py
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  * By Junnan Li
 '''
-import argparse
+#import argparse
 import os
 import ruamel_yaml as yaml
 import numpy as np
